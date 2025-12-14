e100fc71ad12c33ab3decd8386204eeb54cdc5e8
Nguyễn Hữu Bách <114559382+KimiZK-Dev@users.noreply.github.com>
2025-12-14 12:47:52 +0700

    Update youtube-proxy.js

--- api/youtube-proxy.js
+++ api/youtube-proxy.js
@@ -8,6 +8,7 @@ const setCorsHeaders = (res, origin) => {
     'https://kimizk-dev.github.io',
     'http://localhost:3000',
     'http://localhost:8080',
+    'http://localhost:5500',  
     'http://127.0.0.1:3000',
     'http://127.0.0.1:8080',
     'http://127.0.0.1:5500'
